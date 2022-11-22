abac22e3b1b061b9795ed25d0b754396d15db42f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-23 02:55:58 +0530

    SystemUI: Scale mobile data type icon with font size
    The Material You design mobile type icons are just text
    such as "3G" "4G+" "5G" in Google Sans font, as such
    it makes sense to scale it with system font size, and
    it looks more consistent with battery percentage text.
    
    Change-Id: I2e3a985a82c2b9c99a6ceeb5b7eb2110d9bc2038

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarMobileView.java
@@ -190,6 +190,8 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         if (mState.typeId > 0) {
             mMobileType.setContentDescription(mState.typeContentDescription);
             mMobileType.setImageResource(mState.typeId);
+            mMobileType.setScaleX(getContext().getResources().getConfiguration().fontScale);
+            mMobileType.setScaleY(getContext().getResources().getConfiguration().fontScale);
             mMobileType.setVisibility(View.VISIBLE);
         } else {
             mMobile.setVisibility(View.GONE);
@@ -446,6 +448,8 @@ public class StatusBarMobileView extends FrameLayout implements DarkReceiver,
         mMobileType.setVisibility(View.VISIBLE);
         mMobileType.setContentDescription(state.typeContentDescription);
         mMobileType.setImageResource(state.typeId);
+        mMobileType.setScaleX(getContext().getResources().getConfiguration().fontScale);
+        mMobileType.setScaleY(getContext().getResources().getConfiguration().fontScale);
         mMobileTypeSmall.setVisibility(View.GONE);
         setMobileSignalWidth(true);
     }
